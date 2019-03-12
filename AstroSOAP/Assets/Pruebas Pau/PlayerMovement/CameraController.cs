7c83dbca7e57ec93a12a7b9dfa9705e88fd05940
Fenikkel <paufenollosa@gmail.com>
2019-03-12 16:10:38 +0100

    Pau Player controller improvement and some camera features

---
+++ AstroSOAP/Assets/Pruebas Pau/PlayerMovement/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform m_CameraTarget;
+    public Vector3 m_Offset;
+
+    public bool m_UseOffsetValues;
+
+    public float m_RotateSpeed = 5;
+
+    void Start()
+    {
+        if (!m_UseOffsetValues) //si no queremos usar el offset sino donde este la camare en la escena...
+        {
+            m_Offset = m_CameraTarget.position - transform.position;
+        }
+    }
+
+    void Update()
+    {
+        //Get the X position of the mouse & rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * m_RotateSpeed;
+        m_CameraTarget.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * m_RotateSpeed;
+        m_CameraTarget.Rotate(-vertical, 0, 0); //menos para invertir el movimiento de la camara
+
+
+        //Move the camera based on the current rotation of the target & the original offset
+        float desiredYAngle = m_CameraTarget.eulerAngles.y; //euler angles hace que el angulo del quaternion sea el que nosotros entendemos (Osea no tiene en cuenta la W)
+        float desiredXAngle = m_CameraTarget.eulerAngles.x;  //este realmente es el vertical (Mouse Y)
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = m_CameraTarget.position - (rotation * m_Offset); //Asi no funciona --> m_Offset * rotation  (Es al reves)
+
+
+        //transform.position = m_CameraTarget.position - m_Offset; //esto es sin rotacion
+
+        transform.LookAt(m_CameraTarget);
+    }
+}
--- AstroSOAP/Assets/Pruebas Pau/PlayerMovement/PlayerController.cs
+++ AstroSOAP/Assets/Pruebas Pau/PlayerMovement/PlayerController.cs
@@ -7,8 +7,8 @@ public class PlayerController : MonoBehaviour
 {
 
     public float m_MoveSpeed = 10;
-    public float m_JumpForce = 5;
-    public float m_GravityScale = 1; //1 es bastante
+    public float m_JumpForce = 15;
+    public float m_GravityScale = 4; //Controlador de la gravedad
 
     private CharacterController m_PlayerController;
     private Vector3 m_MoveDirection; //En que direccion se meneara el Jugador
@@ -22,13 +22,18 @@ public class PlayerController : MonoBehaviour
 
     void Update() //60fps
     {
+        Debug.Log(m_PlayerController.isGrounded);
+
         SetMovement(); //Configuramos el movimiento
 
-        if (Input.GetButtonDown("Jump"))
+        if (m_PlayerController.isGrounded) //Si esta tocando el suelo
         {
-            Jump(); //Configuramos el salto
+            m_MoveDirection.y = 0; //porque sino estamos aplicando la fuerza de la gravedad todo el rato y el cuerpo pesara un cojon
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump(); //Configuramos el salto
+            }
         }
-
         Move(); //El jugador se mueve con lo configurado
     }
 
@@ -37,8 +42,9 @@ public class PlayerController : MonoBehaviour
         //Coje indistintivamente el Input del teclado o el del mando
         float movHorizontal = Input.GetAxis("Horizontal") * m_MoveSpeed; //GetAxis --> entre -1 y 1
         float movVertical = Input.GetAxis("Vertical") * m_MoveSpeed;
-
-        m_MoveDirection = new Vector3(movHorizontal, 0f, movVertical);
+        
+        m_MoveDirection = new Vector3(movHorizontal, m_MoveDirection.y, movVertical);
+        
     }
 
     private void Jump()
@@ -50,11 +56,16 @@ public class PlayerController : MonoBehaviour
     {
         //Physics.gravity  --->  Edit -> Project Settings -> Physics
 
-        //Aplicamos la gravedad a la configuracion:
-        m_MoveDirection.y = m_MoveDirection.y + (Physics.gravity.y * m_GravityScale); //* Time.deltaTime);  //con Time.DeltaTime tambien se puede pero es un delayer que no podemos modificar
+        /*if (!m_PlayerController.isGrounded) //Aplicamos la gravedad a la configuracion si no estamos tocando el suelo
+        {
+            m_MoveDirection.y = m_MoveDirection.y + (Physics.gravity.y * m_GravityScale * Time.deltaTime); //con Time.DeltaTime hecemos que sea mas smooth y con m_GravityScale podemos controlar la fuerza
+        }*/
+
+        m_MoveDirection.y = m_MoveDirection.y + (Physics.gravity.y * m_GravityScale * Time.deltaTime); //con Time.DeltaTime hecemos que sea mas smooth y con m_GravityScale podemos controlar la fuerza
 
         //Aplicamos el movimiento al jugador
-        m_PlayerController.Move(m_MoveDirection * Time.deltaTime);
+        //HAY QUE HACER UNITARIO EL VECTOR PARA QUE SEA LA MISMA VELOCIDAD PARA TODAS LAS DIRECCIONES??
+        m_PlayerController.Move(m_MoveDirection * Time.deltaTime); //Se mueve hacia donde indica el vector (Supongo que le estamos indicando cuantas unidades se ha de mover en esa direccion, no al punto donde debe ir)
     }
 
 }
