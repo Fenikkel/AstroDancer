c12636f358350d67b71cb2ab66854c26b96d538f
Fenikkel <paufenollosa@gmail.com>
2019-03-12 12:16:19 +0100

    Pau testing player movement

---
+++ AstroSOAP/Assets/Pruebas Pau/PlayerMovement/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float m_MoveSpeed = 10;
+    public float m_JumpForce = 5;
+    public float m_GravityScale = 1; //1 es bastante
+
+    private CharacterController m_PlayerController;
+    private Vector3 m_MoveDirection; //En que direccion se meneara el Jugador
+
+
+    void Start()
+    {
+        m_PlayerController = GetComponent<CharacterController>();
+    }
+
+
+    void Update() //60fps
+    {
+        SetMovement(); //Configuramos el movimiento
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump(); //Configuramos el salto
+        }
+
+        Move(); //El jugador se mueve con lo configurado
+    }
+
+    private void SetMovement()
+    {
+        //Coje indistintivamente el Input del teclado o el del mando
+        float movHorizontal = Input.GetAxis("Horizontal") * m_MoveSpeed; //GetAxis --> entre -1 y 1
+        float movVertical = Input.GetAxis("Vertical") * m_MoveSpeed;
+
+        m_MoveDirection = new Vector3(movHorizontal, 0f, movVertical);
+    }
+
+    private void Jump()
+    {
+        m_MoveDirection.y = m_JumpForce;
+    }
+
+    private void Move()
+    {
+        //Physics.gravity  --->  Edit -> Project Settings -> Physics
+
+        //Aplicamos la gravedad a la configuracion:
+        m_MoveDirection.y = m_MoveDirection.y + (Physics.gravity.y * m_GravityScale); //* Time.deltaTime);  //con Time.DeltaTime tambien se puede pero es un delayer que no podemos modificar
+
+        //Aplicamos el movimiento al jugador
+        m_PlayerController.Move(m_MoveDirection * Time.deltaTime);
+    }
+
+}
---
+++ AstroSOAP/Assets/Pruebas Pau/PlayerMovement/PlayerControllerWithRigidBody.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerWithRigidBody : MonoBehaviour
+{
+
+    public float m_MoveSpeed = 10;
+    public float m_JumpForce = 5;
+
+    private Rigidbody m_PlayerRigidBody;
+
+
+    void Start()
+    {
+        m_PlayerRigidBody = GetComponent<Rigidbody>(); //El objeto tiene que tener el componente RigidBody
+    }
+
+    
+    void Update()
+    {
+        Move();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+    }
+
+    private void Jump()
+    {
+        float velX = m_PlayerRigidBody.velocity.x; //la velocidad en X que ha calculado el rigidbody del jugador
+        float velZ = m_PlayerRigidBody.velocity.z; //mismo en Z
+
+        m_PlayerRigidBody.velocity = new Vector3(velX, m_JumpForce, velZ);
+    }
+
+    private void Move()
+    {
+        //Coje indistintivamente el Input del teclado o el del mando
+        float movHorizontal = Input.GetAxis("Horizontal") * m_MoveSpeed; //GetAxis --> entre -1 y 1
+        float movVertical = Input.GetAxis("Vertical") * m_MoveSpeed;
+
+        m_PlayerRigidBody.velocity = new Vector3(movHorizontal, m_PlayerRigidBody.velocity.y, movVertical); //m_PlayerRigidBody.velocity.y --> no nos movemos en la y, cojemos la que ya esta
+
+    }
+
+}
